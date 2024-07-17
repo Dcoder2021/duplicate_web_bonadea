185265ed1f2f0dbace5fc0070f3a1e7a2b07d990
Dimuthu Epitakaduwa <88632839+Dcoder2021@users.noreply.github.com>
2024-07-17 22:29:22 +0530

    logo action

--- javascript/eventJavascript.js
+++ javascript/eventJavascript.js
@@ -52,3 +52,10 @@ var swiper = new Swiper(".mySwiper", {
     setInterval(nextSlide, 3000);
   });
 
+    window.addEventListener('scroll', function() {
+        const logo = document.querySelector('.masterlogo');
+        const scrollY = window.scrollY || document.documentElement.scrollTop;
+        const scale = 1 - Math.min(scrollY / 1000, 0.5); // Adjust the divisor for zoom speed
+        logo.style.transform = `scale(${scale})`;
+    });
+
